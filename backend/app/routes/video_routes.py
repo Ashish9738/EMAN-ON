76c301a9ae2807a337588f4770bc27aec1a819c9
Ashish9738 <ayadav9738@gmail.com>
2024-12-20 01:30:16 +0530

    tweaked some of the video param

--- backend/app/routes/video_routes.py
+++ backend/app/routes/video_routes.py
@@ -3,10 +3,10 @@ from fastapi.responses import JSONResponse
 from tempfile import NamedTemporaryFile
 import os
 
-from ..models.video_processor import VideoProcessor
+from ..service.video_processor import VideoProcessor
 
 # Path to the pre-trained model
-MODEL_PATH = '/home/ash/Documents/EMAN-ON/models/model_97_acc_100_frames_FF_data.pt'
+MODEL_PATH = '/home/ash/Documents/EMAN-ON/models/video.pt'
 
 # Initialize the VideoProcessor
 try:
@@ -26,7 +26,7 @@ async def predict_video(video: UploadFile):
     # Validate file type
     if not video.content_type.startswith("video"):
         raise HTTPException(status_code=400, detail="Uploaded file is not a video.")
-
+    
     try:
         # Read file contents
         contents = await video.read()
@@ -35,19 +35,20 @@ async def predict_video(video: UploadFile):
         with NamedTemporaryFile(delete=False, suffix=".mp4") as temp_file:
             temp_file.write(contents)
             temp_video_path = temp_file.name
-
+        
         # Perform prediction
         result, confidence, frame_predictions = video_processor.predict_video(temp_video_path)
-
+        print(result, confidence, frame_predictions)
+        
         # Clean up the temporary file
         os.remove(temp_video_path)
-
+        
         # Return the result as JSON
         return JSONResponse(
             content={
                 "result": result,
                 "confidence": confidence,
-                # "frame_predictions": frame_predictions
+                # "frame_predictions": frame_predictions  # Commented out to reduce response size
             }
         )
     except Exception as e:
